d3565114341b0ce2b6363a6c2b010f5db7fb4079
Phil Pursglove <phil@philippursglove.com>
2013-05-23 22:48:37 +0100

    Added Glimpse.AspNet, changed inheritance from ITab to AspNetTab

--- Glimpse.EPiServer/Glimpse.EPiServer/EPiServerPlugin.cs
+++ Glimpse.EPiServer/Glimpse.EPiServer/EPiServerPlugin.cs
@@ -1,8 +1,22 @@
-﻿using Glimpse.Core.Extensibility;
+﻿using Glimpse.AspNet.Extensibility;
+using Glimpse.Core.Extensibility;
 
 namespace Glimpse.EPiServer
 {
-    public class EPiServerPlugin
+    public class EPiServerPlugin : AspNetTab
     {
+        public override object GetData(ITabContext context)
+        {
+            return new object();
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "EPiServer";
+            }
+        }
+
     }
 }
