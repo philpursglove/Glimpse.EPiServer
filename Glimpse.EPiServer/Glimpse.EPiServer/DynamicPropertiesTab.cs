eff95b31264697a02ce3281ddcaefe77e23db12b
Phil Pursglove <phil@philippursglove.com>
2013-05-30 07:35:15 +0100

    Added second tab for dynamic properties

---
+++ Glimpse.EPiServer/Glimpse.EPiServer/DynamicPropertiesTab.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Glimpse.AspNet.Extensibility;
+
+namespace Glimpse.EPiServer
+{
+    class DynamicPropertiesTab : AspNetTab
+    {
+        public override object GetData(Core.Extensibility.ITabContext context)
+        {
+            return new object();
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Dynamic Properties";
+            }
+        }
+    }
+}
