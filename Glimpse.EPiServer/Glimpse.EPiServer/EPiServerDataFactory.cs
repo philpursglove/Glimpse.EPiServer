87b3419a2774fcbf65f4d4130dfbd69f03af038c
Phil Pursglove <phil@philippursglove.com>
2013-05-30 10:02:19 +0100

    Internal class for getting to EPiServer data so it doesn't have to be done in every tab

---
+++ Glimpse.EPiServer/Glimpse.EPiServer/EPiServerDataFactory.cs
@@ -0,0 +1,22 @@
+﻿
+using EPiServer.Core;
+
+using Glimpse.AspNet.Extensions;
+using Glimpse.Core.Extensibility;
+
+namespace Glimpse.EPiServer
+{
+    static class EPiServerDataFactory
+    {
+        static PageData GetPageData(ITabContext context)
+        {
+            PageReference currentPageRef = PageReference.ParseUrl(context.GetHttpContext().Request.RawUrl);
+            return new PageData(currentPageRef);
+        }
+
+        static PageReference GetPageReference(ITabContext context)
+        {
+            return PageReference.ParseUrl(context.GetHttpContext().Request.RawUrl);
+        }
+    }
+}
